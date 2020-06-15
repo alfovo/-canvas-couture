2ea927c27a152fa3814caa30512514095a715c62
alfovo <alex.volpert@lifelink.com>
2020-06-14 18:52:38 -0600

    adding a stats bar and fixing mona lisa, finally!

---
+++ README.md
@@ -0,0 +1,1 @@
+A series of 90s style javascript animations, css transitions and janky little widgets for Jillian.
--- js/nodeTransformations.js
+++ js/nodeTransformations.js
@@ -1,7 +1,19 @@
 function translateDocumentation() {
-  const documentContents = document.body.getElementsByClassName(
-    "documentation"
-  );
+  const documentContents = document.querySelector("#documentation");
+
+  // remove button
+  const button = document.getElementById("documentation-button");
+  button.parentNode.removeChild(button);
+
+  // colors change to something more evil
+  documentContents.style.background = "yellow";
+
+  // translate title
+  const title = documentContents.getElementsByTagName("h2")[0];
+  title.textContent = " ⚠️ Caution";
+  title.style.color = "yellow";
+
+  // translate page
   const translateDict = {
     simple: "poorly documented",
     intuitive: "confusing",
@@ -9,7 +21,7 @@ function translateDocumentation() {
     easy: "impossible to debug",
     "join the conversation": "rely on the kindness of strangers",
   };
-  const textElements = documentContents[0].getElementsByTagName("p");
+  const textElements = documentContents.getElementsByTagName("p");
   for (let text of textElements) {
     for (childNode of text.childNodes) {
       if (childNode.nodeType == 3) {
@@ -29,33 +41,33 @@ function translateDocumentation() {
 }
 
 function initiateCurse() {
-  var h = document.createElement("H1");
-  const warning = document.createTextNode("Jillian! Why didnt you listen?");
-  h.appendChild(warning);
   docTitle = document.body.getElementsByTagName("h1")[0];
-  docTitle.parentNode.replaceChild(h, docTitle);
+  docTitle.textContent = "Jillian! Why didnt you listen?";
+  document.body.style.color = "white";
+  document.body.style.background = "black";
+
+  // remove documentation "game" for now
+  const documention = document.getElementById("documentation");
+  documention.parentNode.removeChild(documention);
 
   let text = document.createTextNode("Jillian, I told you not to click!");
   const items = document.body.getElementsByClassName("grid-item");
-  for (let i = items.length - 1; i >= 0; i--) {
-    let item = items[i];
-    let noun = i == 0 ? "time" : "times";
-    const text = document.createTextNode(
-      `You have been cursed ${i + 1} ${noun}!`
-    );
-    item.parentNode.replaceChild(text, item);
+  for (item of items) {
+    initiateCurse2(item);
   }
 }
 
-function initiateCurse2() {
-  let george = document.querySelector("object");
+function initiateCurse2(george) {
+  console.log(george);
+  george.style.position = "relative";
+  const randomNumber = Math.floor(Math.random() * 80) + 10;
   let angle = Math.PI / 2;
   function animate(time, lastTime) {
     if (lastTime != null) {
       angle += (time - lastTime) * 0.001;
     }
-    george.style.top = Math.sin(angle) * 30 + "px";
-    george.style.left = Math.cos(angle) * 30 + "px";
+    george.style.top = Math.sin(angle) * randomNumber + "px";
+    george.style.left = Math.cos(angle) * randomNumber + "px";
     requestAnimationFrame((newTime) => animate(newTime, time));
   }
   requestAnimationFrame(animate);
